9440f06ddfaed1a3ce540fd65ad87cad77495aae
Quantum <quantum2048@gmail.com>
2018-11-20 19:19:57 -0500

    Fix packaging

--- setup.py
+++ setup.py
@@ -1,24 +1,27 @@
+import os
+
 import setuptools
 
-with open("README.md", "r") as fh:
+
+with open(os.path.join(os.path.dirname(__file__), 'README.md')) as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="pymoss",
-    version="0.0.4",
-    author="Tudor Brindus",
-    author_email="me@tbrindus.ca",
-    description="Library for interfacting with Stanford MOSS.",
+    py_modules=['moss'],
+    name='pymoss',
+    version='0.1',
+    author='Tudor Brindus',
+    author_email='me@tbrindus.ca',
+    description='Library for interfacting with Stanford MOSS.',
     long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/DMOJ/pymoss",
+    long_description_content_type='text/markdown',
+    url='https://github.com/DMOJ/pymoss',
     keywords=['moss', 'plagarism-detection'],
-    packages=setuptools.find_packages(),
     classifiers=[
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 2",
-        "License :: OSI Approved :: GNU Affero General Public License v3",
-        "Operating System :: OS Independent",
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 2',
+        'License :: OSI Approved :: GNU Affero General Public License v3',
+        'Operating System :: OS Independent',
     ],
 )
